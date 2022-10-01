81c417152f85789bcc76e24deee6fff25055eecf
Domilson Firmino <almeidagilbertofirmino@gmail.com>
2022-10-01 21:41:47 +0000

    Single round completed

--- script.js
+++ script.js
@@ -1,28 +1,42 @@
-let PlayerScore;
-let ComputerScore;
 let PlayerChoice;
 let ComputerChoice;
 
 const choices = ["rock","paper","scissor"];
+
 const cards = document.querySelectorAll(".player .card");
+const ComputerScore = document.querySelector(".cScore");
+const PlayerScore = document.querySelector(".pScore");
 
 let selected = false;
 for(let i=0; i<cards.length;i++){
     cards[i].addEventListener("click",function(){
         if(!selected){
             toggleC(cards[i].querySelector(".card__content"));
-            PlayerChoice = cards[i].className.substring(4);
+            PlayerChoice = cards[i].className.substring(5);
             selected = true;
             singleRound();
         }     
     });
 }
 
+
 function toggleC(content){
     content.classList.toggle('isflipped');
 }
 function toggleD(content){
     content.classList.toggle('asflipped');
+}
+function declareWinner(resultado){
+   const a = document.querySelector(".win");
+   a.innerHTML = resultado;
+   a.style.visibility = "visible";
+   updateScore(resultado);
+}
+function updateScore(resultado){
+    if(resultado == "YOU LOSE")
+        ComputerScore.innerHTML = (ComputerScore.innerHTML.charAt(0)-'0')+1;
+    else if(resultado == "YOU WIN")
+        PlayerScore.innerHTML = (PlayerScore.innerHTML.charAt(0)-'0')+1;
 }
 
 function singleRound(){
@@ -31,13 +45,12 @@ function singleRound(){
     
     let card = document.querySelector(a);
     toggleD(card.querySelector(".card__content"));
-    console.log(`You choosed ${PlayerChoice}`);
-    console.log(`Computer Choice is ${ComputerChoice}`);
+
 
     if( PlayerChoice == ComputerChoice)
-        return "Its a Tie";
-    else if(PlayerChoice == "rock" && ComputerChoice=="paper" || PlayerChoice == "scissor" && ComputerChoice=="rock" || PlayerChoice == "paper" && ComputerChoice=="scissor")
-        return "You Lose";
+        declareWinner("IT'S a TIE");
+    else if(PlayerChoice == "rock" && ComputerChoice == "paper" || PlayerChoice == "paper" && ComputerChoice == "scissor" || PlayerChoice == "scissor" && ComputerChoice == "rock")
+        declareWinner("YOU LOSE");
     else
-        return "You Win";
+        declareWinner("YOU WIN");
 };
