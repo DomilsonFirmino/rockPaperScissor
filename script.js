39f795e1f34dcb473999375806c8726902180d61
Domilson Firmino <almeidagilbertofirmino@gmail.com>
2022-10-02 21:36:38 +0000

    Completed Game, missing The Winner  Anoucemment

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
 let PlayerChoice;
 let ComputerChoice;
+let contRounds = 0;
 
 const choices = ["rock","paper","scissor"];
 
 const cards = document.querySelectorAll(".player .card");
 const ComputerScore = document.querySelector(".cScore");
 const PlayerScore = document.querySelector(".pScore");
+const RoundNumber = document.querySelector(".number");
 
 let selected = false;
 for(let i=0; i<cards.length;i++){
     cards[i].addEventListener("click",function(){
-        if(!selected){
+        if(!selected && contRounds < 5){
             toggleC(cards[i].querySelector(".card__content"));
             PlayerChoice = cards[i].className.substring(5);
             selected = true;
             singleRound();
+            contRounds++;
+            setTimeout(reset,1000);
+            RoundNumber.innerHTML = contRounds;
         }     
     });
 }
@@ -46,11 +51,22 @@ function singleRound(){
     let card = document.querySelector(a);
     toggleD(card.querySelector(".card__content"));
 
-
     if( PlayerChoice == ComputerChoice)
         declareWinner("IT'S a TIE");
     else if(PlayerChoice == "rock" && ComputerChoice == "paper" || PlayerChoice == "paper" && ComputerChoice == "scissor" || PlayerChoice == "scissor" && ComputerChoice == "rock")
         declareWinner("YOU LOSE");
     else
         declareWinner("YOU WIN");
-};
+}
+
+function reset(){
+    selected = false;
+    let a = ".computer "; a = a + "."+ComputerChoice; a = a+" .card__content";
+    document.querySelector(a).classList.remove("asflipped");
+
+    document.querySelector(".win").style.visibility = "hidden";
+
+    let b = ".player "; b = b + "."+PlayerChoice; b = b + " .card__content";
+    document.querySelector(b).classList.remove("isflipped");
+
+}
